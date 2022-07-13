ee6a466982842b86870f2637b153ec43ff971000
Philippe Dubois <philippe@voxistech.eu>
2022-07-13 17:06:26 +0200

    Create install_nomad.sh

---
+++ install_nomad.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+
+apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
+apt-get update && apt-get -y install nomad
+
+nomad
